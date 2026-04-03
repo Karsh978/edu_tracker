5734ef89a2c13898b8f9edebece8d6b5159d8429
jiwanlal <jivankarsh2021@gmail.com>
2026-04-04 00:03:29 +0530

    Fixed CORS for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-const API =axios.create({
-    baseURL:'http://localhost:5000/api',
+
+const API = axios.create({
+  baseURL: "https://edutrack-api-8t5g.onrender.com" 
 });
 
-
-API.interceptors.request.use((req)=>{
-    const user =JSON.parse(localStorage.getItem('user'));
-    if(user && user.token){
-        req.headers.Authorization =`Bearer ${user.token}`;
-    }
-     return req;
+API.interceptors.request.use((req) => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  if (user && user.token) {
+    req.headers.Authorization = `Bearer ${user.token}`;
+  }
+  return req;
 });
 
- export default API;
+export default API;
